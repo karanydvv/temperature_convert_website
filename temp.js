ef228778472ea5be713931a7c44d26ab8fbda476
karanydvv <118754730+karanydvv@users.noreply.github.com>
2023-05-29 14:08:16 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,5 @@
+function convertTemp() {
+	var celsius = document.getElementById("celsius").value;
+	var fahrenheit = (celsius * 9/5) + 32;
+	document.getElementById("fahrenheit").value = fahrenheit.toFixed(2);
+}
